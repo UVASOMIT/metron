2a483af81e0fce2a1aeb5840dc875d1d459eadab
Michael Szul <michael@szul.us>
2016-10-10 16:22:32 -0400

    Broke out form information

--- src/metron.forms.ts
+++ src/metron.forms.ts
@@ -4,15 +4,59 @@ namespace metron {
             
         }
     }
-    export abstract class form {
+    export class form<T> {
         private _field_id: string;
         private _name: string;
         private _classNames: Array<string> = [];
         private _action: string = "";
         private _method:string = "POST";
         private _fields: Array<string> = [];
-        constructor() {
-
+        constructor(public model: string, public asscListing: list<T>) {
+            var self = this;
+            self.init();
+        }
+        private init(): void {
+            var self = this;
+            let form: Element = document.selectOne(`[data-m-type='form'][data-m-model='${self.model}']`);
+            let controlBlocks: NodeListOf<Element> = form.selectAll("[data-m-segment='controls']");
+            controlBlocks.each(function (idx: number, elem: Element) {
+                let actions = elem.selectAll("[data-m-action]");
+                actions.each(function (indx: number, el: Element) {
+                    switch (el.attribute("data-m-action").lower()) {
+                        case "submit":
+                            el.addEvent("click", function (e) {
+                                e.preventDefault();
+                                if (metron.form.isValid(`[data-m-type='form'][data-m-model='${self.model}']`)) {
+                                    let parameters: any = { };
+                                    let form: Element = document.selectOne(`[data-m-type='form'][data-m-model='${self.model}']`);
+                                    form.selectAll("input, select, textarea").each(function(idx: number, elem: Element) {
+                                        parameters[<string>elem.attribute("name")] = (<HTMLElement>elem).val();
+                                    });
+                                    metron.web.get(`${metron.fw.getAPIURL(self.model)}`, parameters, null, "json", function (data: T) {
+                                        let form: Element = document.selectOne(`[data-m-type='form'][data-m-model='${self.model}']`);
+                                        (<HTMLElement>form.selectOne(`#${self.model}_${self.model}ID`)).val(<string><any>data[`${self.model}ID`]);
+                                        form.attribute("data-m-state", "hide");
+                                        form.hide();
+                                        if(self.asscListing != null) {
+                                            self.asscListing.callListing();
+                                        }
+                                    });
+                                }
+                            });
+                            break;
+                        case "cancel":
+                            el.addEvent("click", function (e) {
+                                metron.form.clearForm(`[data-m-type='form'][data-m-model='${self.model}']`);
+                                let form: Element = document.selectOne(`[data-m-type='form'][data-m-model='${self.model}']`);
+                                form.attribute("data-m-state", "hide");
+                                form.hide();
+                            });
+                            break;
+                        default:
+                            break;
+                    }
+                });
+            });
         }
         public static clearForm(selector: string, callback?: Function): void {
             document.selectAll(".error").each(function (idx, elem) {
