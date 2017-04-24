77f500926de7b2b3a5457bc65034142540335484
Michael Szul <michael@szul.us>
2017-04-24 15:53:53 -0400

    Removed post processor

--- postprocess.js
+++
@@ -1,9 +0,0 @@
-var exec = require("child_process").exec;
-exec("npm install", function(error, stdout, stderr) {
-    console.log(stdout);
-    console.log(stderr);
-    exec("grunt default", function(error, stdout, stderr) {
-        console.log(stdout);
-        console.log(stderr);
-    });
-});
