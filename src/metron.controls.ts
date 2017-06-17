bde02dc662a225dc62d3c7efdd10e4e6b1715cf7
szul <michael@szul.us>
2017-06-17 10:34:32 -0400

    Preparing for list/form/view usage of pivots

--- src/metron.controls.ts
+++ src/metron.controls.ts
@@ -6,7 +6,12 @@ namespace metron {
             public static bindAll(callback?: Function): void {
                 let pivots : NodeListOf<Element> = document.selectAll("[data-m-type='pivot']");
                 for (let i = 0; i < pivots.length; i++) {
-                    new controls.pivot(<Element>pivots[i]);
+                    let section: Element = <Element>pivots[i];
+                    let page: string = section.attribute("data-m-page");
+                    if (metron.globals["pivots"][page] == null) {
+                        let p: pivot = new controls.pivot(<Element>section);
+                        metron.globals["pivots"][page] = p;
+                    }
                 }
                 if (callback != null) {
                     callback();
@@ -15,18 +20,18 @@ namespace metron {
         }
 
         export class pivot {
-            private _itemContainer: Element;
+            private _items: Element;
             private _item: Pivot;
             private _previousButton: any;
             private _nextButton: any;
-            constructor(private pivotCollection: Element, private displayIndex: number = 0, private nextButton?: any, private previousButton?: any, private eventFunction?: Function, private preEventFunction?: Function){
+            constructor(private pivotCollection: Element, private displayIndex: number = 0, private nextButton?: any, private previousButton?: any, private eventFunction?: Function, private preEventFunction?: Function) {
                 var self = this;
-                self._itemContainer = pivotCollection;
-                self._nextButton = (nextButton != null) ? document.selectOne(`#${nextButton}`): self._itemContainer.selectOne("[data-m-segment='controls'] [data-m-action='next']");
-                self._previousButton = (previousButton != null) ? document.selectOne(`#${previousButton}`): self._itemContainer.selectOne("[data-m-segment='controls'] [data-m-action='previous']");
+                self._items = pivotCollection;
+                self._nextButton = (nextButton != null) ? document.selectOne(`#${nextButton}`): self._items.selectOne("[data-m-segment='controls'] [data-m-action='next']");
+                self._previousButton = (previousButton != null) ? document.selectOne(`#${previousButton}`): self._items.selectOne("[data-m-segment='controls'] [data-m-action='previous']");
                 if (displayIndex != null) {
                     let i = 0;
-                    let itemList = self._itemContainer.selectAll("[data-m-segment='pivot-items'] [data-m-segment='pivot-item']");
+                    let itemList = self._items.selectAll("[data-m-segment='pivot-items'] [data-m-segment='pivot-item']");
                     itemList.each(function (idx: number, elem: Element) {
                         if (idx == displayIndex) {
                             self.init(elem);
@@ -83,7 +88,6 @@ namespace metron {
                 self.init(self._item.next);
                 return true;
             }
-
             private previous(): boolean {
                 var self = this;
                 if (!self._item.previous) {
@@ -93,6 +97,17 @@ namespace metron {
                 self._item.current.toggle();
                 self._item.previous.show();
                 self.init(self._item.previous);
+                return true;
+            }
+            private exact(idx: number): boolean {
+                var self = this;
+                if(!self._items[idx]) {
+                    console.log(`Error: No pivot at index ${idx}`);
+                    return false;
+                }
+                self._item.current.hide();
+                self._items[idx].show();
+                self.init(self._items[idx]);
                 return true;
             }
         }
