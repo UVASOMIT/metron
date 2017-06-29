a6973730aa6b0a172b28e83a0507c57235b8bec2
Ashish Pathak <ashish.pathak@live.com>
2017-06-29 15:48:48 -0400

    added function to add functions

--- src/metron.controls.ts
+++ src/metron.controls.ts
@@ -16,7 +16,6 @@ namespace metron {
                         p._postEventFunctions["hi"] = function(){
                             console.log("Hi");
                         };*/
-                        p.initPivot();
                         metron.globals["pivots"][page] = p;
                     }
                 }
@@ -34,18 +33,15 @@ namespace metron {
             private _item: Pivot;
             private _previousButton: any;
             private _nextButton: any;
-            public _preEventFuntions: EventFunction = {};
-            public _postEventFunctions: EventFunction = {};
+            public preEventFuntions: EventFunction = {};
+            public postEventFunctions: EventFunction = {};
 
             constructor(private pivotCollection: Element, private displayIndex: number = 0, private nextButton?: any, private previousButton?: any, private eventFunction?: Function, private preEventFunction?: Function) {
                 var self = this;
                 self._pivotContainer = pivotCollection;
                 self._nextButton = (nextButton != null) ? document.selectOne(`#${nextButton}`): self._pivotContainer.selectOne("[data-m-segment='controls'] [data-m-action='next']");
                 self._previousButton = (previousButton != null) ? document.selectOne(`#${previousButton}`): self._pivotContainer.selectOne("[data-m-segment='controls'] [data-m-action='previous']");
-            }
 
-            public initPivot(){
-                var self = this;
                 if (self.displayIndex != null) {
                     let i = 0;
                     let itemList = self._pivotContainer.selectAll("[data-m-segment='pivot-item']");
@@ -54,11 +50,6 @@ namespace metron {
                         if (idx == self.displayIndex) {
                             self.init(elem);
                             elem.show();
-                            if (elem.attribute('data-m-page') != null){
-                                if (self._postEventFunctions[elem.attribute('data-m-page')] != undefined){
-                                    self._postEventFunctions[elem.attribute('data-m-page')]();
-                                }
-                            }
                         }
                         else {
                             elem.hide();
@@ -66,13 +57,14 @@ namespace metron {
                     });
                 }
             }
-            private init(item: Element) {
+            
+            private init(el: Element) {
                 var self = this;
                 self._item = {
-                    parent: item.parentElement,
-                    current: item,
-                    next: (item.nextElementSibling != null && item.nextElementSibling.attribute("data-m-segment") != null && item.nextElementSibling.attribute("data-m-segment") == "pivot-item") ? item.nextElementSibling : null,
-                    previous: (item.previousElementSibling != null && item.previousElementSibling.attribute("data-m-segment") != null && item.previousElementSibling.attribute("data-m-segment") == "pivot-item") ? item.previousElementSibling : null,
+                    parent: el.parentElement,
+                    current: el,
+                    next: (el.nextElementSibling != null &&el.nextElementSibling.attribute("data-m-segment") != null && el.nextElementSibling.attribute("data-m-segment") == "pivot-item") ? el.nextElementSibling : null,
+                    previous: (el.previousElementSibling != null && el.previousElementSibling.attribute("data-m-segment") != null && el.previousElementSibling.attribute("data-m-segment") == "pivot-item") ? el.previousElementSibling : null,
                 };
                 if (self._nextButton != null || self._previousButton != null) {
                     if (self._item.next == null) {
@@ -96,8 +88,12 @@ namespace metron {
                         }, true);
                     }
                 }
-            }
-            private applyActionEvents(el: Element) {
+
+                if (self._item.current.attribute('data-m-page') != null){
+                    if (self.postEventFunctions[self._item.current.attribute('data-m-page')] != undefined){
+                        self.postEventFunctions[self._item.current.attribute('data-m-page')]();
+                    }
+                }
             }
             public next(): boolean {
                 var self = this;
@@ -105,19 +101,9 @@ namespace metron {
                     console.log("Couldn't find next pivot");
                     return false;
                 }
-                if (self._item.current.attribute('data-m-page') != null){
-                    if (self._preEventFuntions[self._item.current.attribute('data-m-page')] != undefined){
-                        self._preEventFuntions[self._item.current.attribute('data-m-page')]();
-                    }
-                    
-                }
+                self.applyPreEvent(self._item.current);
                 self._item.current.toggle();
                 self._item.next.show();
-                if (self._item.next.attribute('data-m-page') != null){
-                    if (self._postEventFunctions[self._item.next.attribute('data-m-page')] != undefined){
-                        self._postEventFunctions[self._item.next.attribute('data-m-page')]();
-                    }
-                }
                 self.init(self._item.next);
                 return true;
             }
@@ -127,18 +113,9 @@ namespace metron {
                     console.log("Couldn't find previous pivot");
                     return false;
                 }
-                if (self._item.current.attribute('data-m-page') != null){
-                    if (self._preEventFuntions[self._item.current.attribute('data-m-page')] != undefined){
-                        self._preEventFuntions[self._item.current.attribute('data-m-page')]();
-                    }
-                }
+                self.applyPreEvent(self._item.current);
                 self._item.current.toggle();
                 self._item.previous.show();
-                if (self._item.previous.attribute('data-m-page') != null){
-                    if (self._postEventFunctions[self._item.previous.attribute('data-m-page')] != undefined){
-                        self._postEventFunctions[self._item.previous.attribute('data-m-page')]();
-                    }
-                }
                 self.init(self._item.previous);
                 return true;
             }
@@ -148,20 +125,20 @@ namespace metron {
                     console.log(`Error: No pivot at index ${idx}`);
                     return false;
                 }
-                if (self._item.current.attribute('data-m-page') != null){
-                    if (self._preEventFuntions[self._item.current.attribute('data-m-page')] != undefined){
-                        self._preEventFuntions[self._item.current.attribute('data-m-page')]();
-                    }
-                }
+                self.applyPreEvent(self._item.current);
                 self._item.current.hide();
                 self._items[idx].show();
-                if (self._items[idx].attribute('data-m-page') != null){
-                    if (self._postEventFunctions[self._items[idx].attribute('data-m-page')] != undefined){
-                        self._postEventFunctions[self._items[idx].attribute('data-m-page')]();
-                    }
-                }
                 self.init(self._items[idx]);
                 return true;
+            }
+            private applyPreEvent(el: Element) {
+                var self = this;
+                if (el.attribute('data-m-page') != null){
+                    if (self.preEventFuntions[el.attribute('data-m-page')] != undefined){
+                        self.preEventFuntions[el.attribute('data-m-page')]();
+                    }
+                    
+                }
             }
         }
     }
