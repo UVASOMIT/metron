66c9948fe1188968b64b15880ff6a89aac8c8714
Michael Szul <michael@szul.us>
2017-09-15 14:57:16 -0400

    Injection change

--- src/metron.base.ts
+++ src/metron.base.ts
@@ -16,9 +16,6 @@ namespace metron {
             if (func == null) {
                 throw new Error("Error: No function passed for injection!");
             }
-            if ((<any>self)[method] == null) {
-                throw new Error(`Error: [${method}] does not exist!`);
-            }
             switch (type.lower()) {
                 case "append":
                     (<any>self)[`${method}_m_inject`] = func;
